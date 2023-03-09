e4a53977eeea0687316b8d54fbaf4a84a84ba161
xiongchao <xiongluchao@163.com>
2023-03-09 17:08:46 +0800

    添加代码1

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -3,5 +3,6 @@ package org.example;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("lllllllllllllll");
     }
 }
