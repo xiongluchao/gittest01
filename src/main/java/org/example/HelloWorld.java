2f8c8e71e0d62602c40c0872bdbb426705d1ad16
xiongchao <xiongluchao@163.com>
2023-03-09 17:10:22 +0800

    修改代码1为2

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -3,6 +3,6 @@ package org.example;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("lllllllllllllll");
+        System.out.println("2222222222222");
     }
 }
