192ca4abb829d9cd1762de5c19419d5cfb4c45b3
xiongchao <xiongluchao@163.com>
2023-03-09 17:15:38 +0800

    修改修改代码3

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -3,6 +3,6 @@ package org.example;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("222333333332");
+        System.out.println("hello scala");
     }
 }
